2ee9137753b20f8d29d0fbeae9e8c0fe9b14ed70
prajwal k <prajwal11082000@gmail.com>
2025-11-11 13:11:08 +0530

    Added single launch file

--- ros2_ws/src/my_first_pkg/launch/talker_listener.launch.py
+++ ros2_ws/src/my_first_pkg/launch/talker_listener.launch.py
@@ -1,19 +1,17 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
     return LaunchDescription([
         Node(
             package='my_first_pkg',
             executable='talker',
-            name='talker',
-            output='screen'
+            name='talker'
         ),
         Node(
             package='my_first_pkg',
             executable='listener',
-            name='listener',
-            output='screen'
-        )
+            name='listener'
+        ),
     ])
-
--- ros2_ws/src/my_first_pkg/setup.py
+++ ros2_ws/src/my_first_pkg/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'my_first_pkg'
 
@@ -10,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/talker_listener_launch.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
